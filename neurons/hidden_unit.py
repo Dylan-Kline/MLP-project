776153cbf8d603e1863cef2a5d5f53d57576085c
Dylan-Kline <klinedylan46@gmail.com>
2024-03-07 16:00:08 -0500

    working on hidden unit class

--- neurons/hidden_unit.py
+++ neurons/hidden_unit.py
@@ -15,3 +15,13 @@ class HiddenUnit:
             Computes the output of the neuron unit using its incoming connections and the given input numpy array.
             Returns the output from a tanh function on the input array.
             """
+        return self.tanh(np.dot(self.incoming_connections, input))
+
+    @staticmethod
+    def tanh(z: NDArray):
+        '''
+            Computes the tanh function on a numpy array z.
+            @ z : numpy array from the dot product of weights and input data
+            @ return : output from tanh function
+            '''
+        return (np.exp(z) - np.exp(-z)) / (np.exp(z) + np.exp(-z))
