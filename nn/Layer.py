615a1176add8ee1f8d7f4914ed92cb7b08b582e9
Dylan-Kline <klinedylan46@gmail.com>
2024-03-10 23:05:41 -0500

    working on layer class

--- nn/Layer.py
+++ nn/Layer.py
@@ -4,17 +4,55 @@ from numpy.typing import NDArray
 class NeuronLayer:
 
     def __init__(self, num_incoming_connections, num_neurons, activation_func, deriv_activation):
-        self.weights = np.random.randn(num_neurons, num_incoming_connections) 
+        self.weights = np.random.randn(num_neurons, num_incoming_connections) # num units in layer X num weights to layer
         self.activation_func = activation_func
         self.deriv_func = deriv_activation
 
     def forward(self, input: NDArray):
+        '''
+            Performs feed-forward propagation with the given input
+            @input : numpy array of input data
+            return : output from the model based on the given input
+            '''
+        
         self.input = input
         self.output = self.activation_func(np.dot(self.weights, self.input))
         return self.output
     
-    def backward(self, output_error, learning_rate):
+    def backward(self, output_error: NDArray, learning_rate: float):
+        '''
+            Performs delta error backpropagation for the current layer.
+            @ output_error : errors from the following layer
+            @ learning_rate: learning rate of the model
+            return : array of errors for previous layer input
+            '''
 
         # compute the error and gradient for the current layer 
         error = self.deriv_func(self.output) * output_error
         gradient = np.dot(error, self.input.T)
+
+        # update the weights for this layer
+        self.weights -= learning_rate * gradient
+
+        # calculate input error for previous layer
+        input_error = np.dot(self.weights.T, error)
+        return input_error
+    
+    @staticmethod
+    def tanh(z: NDArray):
+        '''
+            Computes the tanh function on a numpy array z.
+            @ z : numpy array from the dot product of weights and input data
+            @ return : output from tanh function
+            '''
+        return (np.exp(z) - np.exp(-z)) / (np.exp(z) + np.exp(-z))
+    
+    @staticmethod
+    def tanh_derivative(z: NDArray):
+        '''
+            Compute the tanh derivative on the given input array z.
+            @ z : numpy array
+            return : output from tanh prime
+            '''
+        
+        return 1 - NeuronLayer.tanh(z) ** 2
