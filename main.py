fixed issue with loan dataset by normalizing the data

main.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 
 def main():
     
-    path = "data/water.train"
+    path = "data/loans.train"
     validation = "data/water.val"
 
     X, T = loadDataset(path)
@@ -14,25 +14,29 @@ def main():
     T = toHotEncoding(T, 2)
     T_val = toHotEncoding(T_val, 2)
 
-    # X = np.array([1.5])
+    X = normailze_data(X)
+
+
+    # X = np.array([1.5, 2.1])
     # # Reshape X to be a 2D array with 1 row and 3 columns
     # X = X.reshape(1, -1)
     # T = np.array([1.0])
 
     # print(X.shape)
 
-    #T = toHotEncoding(T, 2)
+    # T = toHotEncoding(T, 2)
+    # print(T)
     
-    model = LogisticRegression()
-    model.fit(X, T)
-    Y = model.predict(X_val)
-    acc_train = accuracy(T_val, Y)
-    print(acc_train)
+    # model = LogisticRegression()
+    # model.fit(X, T)
+    # Y = model.predict(X)
+    # acc_train = accuracy(T, Y)
+    # print(acc_train)
 
     mlp_model = MultilayerPerceptron()
     mlp_model.fit(X, T)
-    Y = mlp_model.predict(X_val)
-    acc_train2 = accuracy(T_val, Y)
+    Y = mlp_model.predict(X)
+    acc_train2 = accuracy(T, Y)
     print(acc_train2)
     
 main() 

nn/NeuronLayer.py
@@ -6,7 +6,6 @@ class NeuronLayer:
 
     def __init__(self, num_incoming_connections, num_neurons, activation_func, deriv_activation=None):
         self.weights = np.random.randn(num_neurons, num_incoming_connections + 1) # num units in layer X num weights to layer, +1 for bias
-        print("Layer weights: ", self.weights)
         self.activation_func = activation_func
         self.deriv_func = deriv_activation
 
@@ -18,6 +17,7 @@ class NeuronLayer:
             '''
         # Add a column of ones to the input data for the bias
         bias_input = np.hstack([input, np.ones((input.shape[0], 1))])
+        #print(bias_input)
 
         self.input = bias_input
         self.output = self.activation_func(np.dot(self.input, self.weights.T))
@@ -36,12 +36,12 @@ class NeuronLayer:
         gradient = np.dot(error.T, self.input)
 
         # calculate input error for previous layer
-        input_error = np.dot(error, self.weights)
+        input_error = np.dot(error, self.weights[:, :-1])
 
         # update the weights for this layer
         self.weights -= learning_rate * gradient
 
-        return input_error[:, :-1]
+        return input_error
     
     @staticmethod
     def tanh(z: NDArray):
@@ -61,7 +61,7 @@ class NeuronLayer:
             return : output from tanh prime
             '''
         
-        return 1 - NeuronLayer.tanh(z) ** 2
+        return (1 - (NeuronLayer.tanh(z) ** 2))
     
     def print_weights(self):
         print(self.weights.shape)

nn/OutputLayer.py
@@ -18,10 +18,10 @@ class OutputLayer(NeuronLayer):
         gradient = np.dot(output_error.T, self.input)
 
         # input error
-        input_error = np.dot(output_error, self.weights)
+        input_error = np.dot(output_error, self.weights[:, :-1])
 
         # update the weights for this layer
         self.weights -= learning_rate * gradient
 
         # calculate input error for previous layer
-        return input_error[:, :-1]
+        return input_error